0a8066be4dfc37bc823fdc7c84b7174a8e996638
Meshal <63318750+x1LesHx@users.noreply.github.com>
2023-05-15 10:14:39 +0300

    Update index.php

--- docs/FhS_1/index.php
+++ docs/FhS_1/index.php
@@ -1,20 +1,61 @@
 
 <!DOCTYPE HTML>
-<html>
+<html dir="rtl">
 	<head>
 		<title>FhS System</title>
+		<meta content="width=device-width, initial-scale=1.0" name="viewport">
 		<link rel="stylesheet" type="text/css" href="css/style.css"  media="all" />
+		<link href="css/bootstrap.min.css" rel="stylesheet">
 				  <meta charset="utf-8">
+				      <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+	<!-- Google Web Fonts -->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Barlow:wght@600;700&family=Ubuntu:wght@400;500&display=swap" rel="stylesheet"> 
+
+	<!-- Icon Font Stylesheet -->
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+	<!-- Libraries Stylesheet -->
+	<link href="lib/animate/animate.min.css" rel="stylesheet">
+	<link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+	<link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+
+	<!-- Customized Bootstrap Stylesheet -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+
+	<!-- Template Stylesheet -->
+	<link href="css/style.css" rel="stylesheet">
 	</head>
 	<body>
-			<div class="header">
+			<!--<div class="header">
 				<div class="wrap">
 				<div class="logo">
 					<a href="index.html" style="font-size: 30px;">الفحص الدوري</a>
-				</div>
-				
+				</div> -->
+				<!-- new header -->
 			</div>
+			<nav class="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
+        <a href="" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
+            <h3 class="m-0 text-primary"><i class="fa fa-car me-3"></i>الفحص الدوري</h3>
+        </a>
+        <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav ms-auto p-4 p-lg-0">
+                <a href="index.php" class="nav-item nav-link"><b>الرئيسيه</b></a>
+                <a href="about123.html" class="nav-item nav-link active">من نحن</a>
+                <a href="login.php" class="nav-item nav-link">تسجيل الدخول</a>
+                <a href="driverlogin.php" class="nav-item nav-link">تسجيل دخول مقدمي الخدمه</a>
+            </div>
+        </div>
+    </nav>
 		</div>
+			<!-- new header end -->
 					</div>
 		    <div class="clear"> </div>
 		    <div class="content-grids">
@@ -59,6 +100,51 @@
 		    </div>
 		   </div>
 		<center>   <img src="images/car.png" ></center>
+
+		 <!-- Footer Start -->
+		 <div class="container-fluid bg-dark text-light footer pt-5 mt-5 wow fadeIn" data-wow-delay="0.1s">
+        <div class="container py-5">
+            <div class="row g-5">
+                <div class="col-lg-3 col-md-6">
+                    <h4 class="text-light mb-4">الموقع</h4>
+                    <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>الرياض</p>
+                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>009660000000</p>
+                    <p class="mb-2"><i class="fa fa-envelope me-3"></i>tvtc@tvtc.edu</p>
+                    <div class="d-flex pt-2">
+                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
+                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h4 class="text-light mb-4">اوقات العمل</h4>
+                    <h6 class="text-light">الاحد - الخميس:</h6>
+                    <p class="mb-4">09.00 ص - 09.00 م</p>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="position-relative mx-auto" style="max-width: 400px;">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="copyright">
+                <div class="row">
+                    <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                          جميع الحقوق المحفوظه لدى مجموعه فحص  &copy;
+                    </div>
+                    <div class="col-md-6 text-center text-md-end">
+                        <div class="footer-menu">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer End -->
+
+
 	</body>
 </html>
 
