baa237584d238600c73e76eb71e5d27a99df500b
Sultantan353 <Sultantan353@hotmail.com>
2023-05-16 13:19:33 +0300

    add

--- docs/FhS_1/index.php
+++ docs/FhS_1/index.php
@@ -1,6 +1,6 @@
 
 <!DOCTYPE HTML>
-<html dir="rtl">
+<html>
 	<head>
 		<style>
 			 body { color: #ccc }
@@ -982,7 +982,6 @@ textarea, label {
 
 			</style>
 		<title>FhS System</title>
-<<<<<<< HEAD
 		<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -990,105 +989,27 @@ textarea, label {
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
 
-=======
-		<meta content="width=device-width, initial-scale=1.0" name="viewport">
->>>>>>> aff45d1d2df6a09eda1a6301ff875c5dc9c4ccd6
 		<link rel="stylesheet" type="text/css" href="css/style.css"  media="all" />
-		<link href="css/bootstrap.min.css" rel="stylesheet">
 				  <meta charset="utf-8">
-<<<<<<< HEAD
           
 	</head>
 	<body>
 	
 
-=======
-				      <!-- Favicon -->
-    <link href="img/favicon.ico" rel="icon">
-
-	<!-- Google Web Fonts -->
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Barlow:wght@600;700&family=Ubuntu:wght@400;500&display=swap" rel="stylesheet"> 
-
-	<!-- Icon Font Stylesheet -->
-	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
-
-	<!-- Libraries Stylesheet -->
-	<link href="lib/animate/animate.min.css" rel="stylesheet">
-	<link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-	<link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
-
-	<!-- Customized Bootstrap Stylesheet -->
-	<link href="css/bootstrap.min.css" rel="stylesheet">
-
-	<!-- Template Stylesheet -->
-	<link href="css/style.css" rel="stylesheet">
-	</head>
-	<body>
->>>>>>> aff45d1d2df6a09eda1a6301ff875c5dc9c4ccd6
 			<!--<div class="header">
 				<div class="wrap">
 				<div class="logo">
 					<a href="index.html" style="font-size: 30px;">الفحص الدوري</a>
-				</div> -->
-				<!-- new header -->
+				</div>
+				
 			</div>
-			<nav class="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
-        <a href="" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
-            <h3 class="m-0 text-primary"><i class="fa fa-car me-3"></i>الفحص الدوري</h3>
-        </a>
-        <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-            <div class="navbar-nav ms-auto p-4 p-lg-0">
-                <a href="index.php" class="nav-item nav-link"><b>الرئيسيه</b></a>
-                <a href="about123.html" class="nav-item nav-link active">من نحن</a>
-                <a href="login.php" class="nav-item nav-link">تسجيل الدخول</a>
-                <a href="driverlogin.php" class="nav-item nav-link">تسجيل دخول مقدمي الخدمه</a>
-            </div>
-        </div>
-    </nav>
 		</div>
-<<<<<<< HEAD
 					</div>-->
           <div class="header" style="background-color: #1e2530;">
 				<div class="wrap">
 				<!--start-logo-->
 				<div class="logo">
 					<a href="index.html" style="font-size: 30px;color:white">الفحص الدوري</a>
-=======
-			<!-- new header end -->
-					</div>
-		    <div class="clear"> </div>
-		    <div class="content-grids">
-		    	<div class="wrap">
-		    	<div class="section group">
-								
-							
-				<div class="listview_1_of_3 images_1_of_3">
-					<div class="listimg listimg_1_of_2">
-						  <img src="images/q.png" height="117">
-					</div>
-					<div class="text list_1_of_2">
-						  <h3>العملاء</h3>
-						  <p>تسجيل الدخول & طلب خدمة </p>
-						  <div class="button"><span><a href="login.php">تسجيل الدخول</a></span></div>
-				    </div>
-				</div>	
-
-				<div class="listview_1_of_3 images_1_of_3">
-					<div class="listimg listimg_1_of_2">
-						  <img src="images/qq.png">
-					</div>
-					<div class="text list_1_of_2">
-						  <h3>تسجيل دخول مقدمين الخدمة</h3>
-						
-						  <div class="button"><span><a href="driverlogin.php">تسجيل الدخول</a></span></div>
-					</div>
->>>>>>> aff45d1d2df6a09eda1a6301ff875c5dc9c4ccd6
 				</div>
 				<!--end-logo-->
 				<!--start-top-nav-->
@@ -1185,7 +1106,6 @@ textarea, label {
                                     
                                     
 		<center>   <img src="images/car.png" ></center>
-<<<<<<< HEAD
     <div class="wrap">
 		   <div class="content-box"><h1 style="
 			text-align: center;
@@ -1252,53 +1172,6 @@ textarea, label {
 
 
 <!-- Credit to https://bootsnipp.com/snippets/ooa9M -->
-=======
-
-		 <!-- Footer Start -->
-		 <div class="container-fluid bg-dark text-light footer pt-5 mt-5 wow fadeIn" data-wow-delay="0.1s">
-        <div class="container py-5">
-            <div class="row g-5">
-                <div class="col-lg-3 col-md-6">
-                    <h4 class="text-light mb-4">الموقع</h4>
-                    <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>الرياض</p>
-                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>009660000000</p>
-                    <p class="mb-2"><i class="fa fa-envelope me-3"></i>tvtc@tvtc.edu</p>
-                    <div class="d-flex pt-2">
-                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
-                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <h4 class="text-light mb-4">اوقات العمل</h4>
-                    <h6 class="text-light">الاحد - الخميس:</h6>
-                    <p class="mb-4">09.00 ص - 09.00 م</p>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="position-relative mx-auto" style="max-width: 400px;">
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="container">
-            <div class="copyright">
-                <div class="row">
-                    <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                          جميع الحقوق المحفوظه لدى مجموعه فحص  &copy;
-                    </div>
-                    <div class="col-md-6 text-center text-md-end">
-                        <div class="footer-menu">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Footer End -->
-
-
->>>>>>> aff45d1d2df6a09eda1a6301ff875c5dc9c4ccd6
 	</body>
 </html>
 
